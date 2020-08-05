b4d104c0db784fdeda01088a3390f19fab5dd591
Priyanshu Dasgupta <62264051+priyanshudasgupta8@users.noreply.github.com>
2020-08-05 15:41:48 -0700

    Add files via upload

--- Cell.js
+++ Cell.js
@@ -1,5 +1,6 @@
 class Cell {
     constructor(i, j, w) {
+        // t = this;
         this.i = i;
         this.j = j;
         this.x = i * w;
@@ -7,13 +8,22 @@ class Cell {
         this.w = w;
         this.neighbourCount = 0;
 
-        this.image = loadImage('mine.jpg')
+        this.explosion = loadSound('Explosion+1.mp3');
 
-        if (random(1) < 0.5) {
+        if (random(1) < 0.25) {
             this.mine = true;
         } else {
             this.mine = false;
         }
+
+        if (mode == "Extreme" || mode == "extreme") {
+            if (random(1) < 0.2) {
+                this.mine = true;
+            } else {
+                this.mine = false;
+            }
+        }
+
         this.revealed = false;
     }
 
@@ -26,7 +36,7 @@ class Cell {
                 fill('red');
                 ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
             } else {
-                fill('lightgreen');
+                fill('skyblue');
                 noStroke();
                 rect(this.x-1, this.y-1, this.w+1, this.w+1);
                 textAlign(CENTER);
@@ -39,7 +49,7 @@ class Cell {
                 } else if (this.neighborCount == 4) {
                     fill('hotpink')
                 } else if (this.neighborCount == 0) {
-                    fill('lightgreen')
+                    fill('skyblue')
                 } else {
                     fill('red');
                 }
@@ -78,22 +88,48 @@ class Cell {
 
     reveal() {
         this.revealed = true;
+
+        if (this.neighborCount == 0){
+            this.floodFill();
+        }
     }
 
-    // colorize() {
-    //     fill(100);
-    //     noStroke();
-    //     rect(this.x, this.y, this.w, this.w);
-    // }
+    floodFill = function() {
+        for (var xoff = -1; xoff <= 1; xoff++) {
+          var i = this.i + xoff;
+          if (i < 0 || i >= cols) continue;
+      
+          for (var yoff = -1; yoff <= 1; yoff++) {
+            var j = this.j + yoff;
+            if (j < 0 || j >= rows) continue;
+      
+            var neighbor = grid[i][j];
+            // Note the neighbor.bee check was not required.
+            // See issue #184
+            if (!neighbor.revealed) {
+              neighbor.reveal();
+            }
+          }
+        }
+      };
 
-    // updateScore(score) {
-    //     if(mousePressed() && grid[i][j].contains(mouseX, mouseY) && this.mine == false) {
-    //         score++;
-    //         console.log(score);
-    //     } else if (mousePressed() && grid[i][j].contains(mouseX, mouseY) && this.mine == true) {
-    //         score -= 5;
-    //         console.log(score);
-    //     }
-    // }    
+
+
+// /*  colorize() {
+//         fill(100);
+//         noStroke();
+//         rect(this.x, this.y, this.w, this.w);
+//     }
+
+//     updateScore(score) {
+//         if(mousePressed() && grid[i][j].contains(mouseX, mouseY) && this.mine == false) {
+//             score++;
+//             console.log(score);
+//         } else if (mousePressed() && grid[i][j].contains(mouseX, mouseY) && this.mine == true) {
+//             score -= 5;
+//             console.log(score);
+//         }
+//     }    
+// }
+//
 }
-
--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 var mineSound, billyPlay, winMp3;
 var grid;
+var bg;
+var score = 100;
 
 var cols;
 var rows;
 var w = 30;
+
+let GameState = 'play';
+
+var mode = prompt("Which mode do you want to play in (Easy / Medium / Hard / Extreme) USE GRAMMATICAL RULES");
 
 function make2DArray(cols, rows) {
   var arr = new Array(cols);
@@ -13,17 +19,36 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
-var options = [];
+function preload() {
+  bg = loadImage("background.jpg");
+}
 
+var options = [];
+let canvas;
 
 function setup() {
   mineSound = loadSound('Explosion+1.mp3');
   billyPlay = loadSound('endState.mp3');
   winMp3 = loadSound('scenario.mp3');
-  var canvas = createCanvas(150, 150);
+
+  if (mode == "easy" || mode == "Easy"){
+    canvas = createCanvas(210, 210);
+    canvas.center();
+  } else if (mode == "medium" || mode == "Medium") {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+  } else if (mode == "Hard" || mode == "hard") {
+    canvas = createCanvas(450, 450);
+    canvas.center();
+  } else if (mode == "Extreme" || mode == "extreme") {
+    canvas = createCanvas(windowWidth, windowHeight);
+  } else {
+    canvas = createCanvas(210, 210);
+    canvas.center();
+  }
 
   cols = floor(width/w);
-  rows = floor(height/w);
+  rows = floor((height-10)/w);
 
   grid = make2DArray(cols, rows);
   for (var i = 0; i < cols; i++) {
@@ -39,38 +64,44 @@ function setup() {
 }
 
 function draw() {
-  background(150);
+  if (GameState == 'play') {
+    background('turquoise');
 
-  // for (var i = 0; i < cols; i++) {
-  //   for (var j = 0; j < rows; j++) {
-  //     grid[i][j].updateScore();
-  //   }
-  // }
-
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].display();
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        grid[i][j].display();
+      }
     }
+
+    textSize(25);
+    fill(0);
+    text(`Score: ${score}`, width - (width - 60), height - 7.5);
   }
- 
+
+  if (GameState == 'end') {
+    background('red');
+
+    fill('blue');
+    textSize(15);
+    text("Game Over \n You Stepped on a Mine \n Make Better and \n Thought-through \n Decisions Next Time \n \n Reload to Play Again", width/2, (height/2) - 50)
+  }
 }
 
 function mousePressed(event) {
+  if (event.button == 0) {
     for (var i = 0; i < cols; i++) {
       for (var j = 0; j < rows; j++) {
         if (grid[i][j].contains(mouseX, mouseY)) {
           grid[i][j].reveal();
+          if (!grid[i][j].mine) {
+            score++;
+          } else {
+            score = 0;
+            GameState = 'end';
+          }
         }
       }
     }
+  }
+  
 }
-
-// function mousePressed() {
-//   for (var i = 0; i < cols; i++) {
-//     for (var j = 0; j < rows; j++) {
-//       if (grid[i][j].contains(mouseX, mouseY)) {
-//         grid[i][j].colorize();
-//       }
-//     }
-//   }
-// }
